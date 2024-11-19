f0f465488f7818aa07759f96c8462dfa11d0261b
Elyrah408 <e.rahimi2019@gmail.com>
2024-11-19 08:35:43 +0100

    Add files via upload

---
+++ html css/js/index.js
@@ -0,0 +1,12 @@
+// let close_btn = document.getElementById("navlinks");
+// let open_btn  = document.getElementById("burger");
+
+let navlinks = document.getElementById("navlinks");
+
+function hidemenue(){
+    navlinks.style.right="-200px";
+}
+
+function showmenue(){
+    navlinks.style.right="0";
+}
